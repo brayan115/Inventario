6988607a15b17eb2e7f2b20ae43809d865feb247
brayan115 <brayan.blas@tecsup.edu.pe>
2021-03-28 13:37:11 -0500

    first commit

---
+++ app/src/main/java/com/tienda/minventario/MainActivity.kt
@@ -0,0 +1,37 @@
+package com.tienda.minventario
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import com.google.android.material.bottomnavigation.BottomNavigationView
+import com.tienda.minventario.fragments.AddFragment
+import com.tienda.minventario.fragments.CalculoFragment
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        val addFragment = AddFragment()
+        val calculoFragment = CalculoFragment()
+        val blankFragment = BlankFragment()
+        val bottomNavigation: BottomNavigationView = findViewById(R.id.bottom_navigation)
+
+
+         bottomNavigation.setOnNavigationItemSelectedListener  {
+                 item -> when (item.itemId){
+                R.id.page_1 -> makeCurrentFragment(addFragment)
+                R.id.page_2 -> makeCurrentFragment(calculoFragment)
+                R.id.page_3 -> makeCurrentFragment(blankFragment)
+            }
+            true
+        }
+
+    }
+    private fun makeCurrentFragment(fragment: Fragment)=
+
+        supportFragmentManager.beginTransaction().apply {
+            replace(R.id.content_framelayout,fragment)
+            commit()
+        }
+}
